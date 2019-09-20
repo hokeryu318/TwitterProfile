b74a8024651c45a3d7868625f4f1272d855ef64d
jiuhejong <hokelucpy@hotmail.com>
2019-09-21 07:36:19 +0800

    0920-31

--- app/Http/Controllers/InterviewController.php
+++ app/Http/Controllers/InterviewController.php
@@ -215,8 +215,8 @@ class InterviewController extends Controller
 
         $user_id = Auth::id();
         $user = User::find($user_id);
-//        Twitter::postTweet(array('status' => $user->url.'?sl='.rand(1, 100), 'format' => 'json'));
-        return Twitter::postTweet(array('status' => $user->url, 'format' => 'json'));
+        Twitter::postTweet(array('status' => $user->url.'?sl='.rand(1, 100), 'format' => 'json'));
+//        return Twitter::postTweet(array('status' => $user->url, 'format' => 'json'));
 
 //        return 'tweet success';
     }
