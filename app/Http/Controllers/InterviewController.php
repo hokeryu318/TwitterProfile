73816064ddec2002812a1163646dd5ac1d7ab2e9
jiuhejong <hokelucpy@hotmail.com>
2019-09-21 05:12:19 +0800

    0920-31

--- app/Http/Controllers/InterviewController.php
+++ app/Http/Controllers/InterviewController.php
@@ -212,6 +212,6 @@ class InterviewController extends Controller
 
     public function tweet() {
 
-        return Twitter::postTweet(array('status' => '<a>Laravel Tweet1.</a>', 'format' => 'json'));
+        return Twitter::postTweet(array('status' => '<a>Laravel Tweet1.</a>', 'format' => 'html'));
     }
 }
