563870f6dd23678a9dcb65eef45f241fd39d9f6e
jiuhejong <hokelucpy@hotmail.com>
2019-09-21 04:36:35 +0800

    0920-31

--- resources/views/query/query_sample.blade.php
+++ resources/views/query/query_sample.blade.php
@@ -1,6 +1,6 @@
 @include('layout.page_header')
 
-<div id="interview_post" class="interview_post" style="display: none;">インタビューを送信しました！</div>
+
 
 <div class="container text-center">
 
@@ -9,6 +9,7 @@
         <div class="col-sm-4"></div>
 
         <div class="col-sm-4 pl-40 pr-40">
+            <div id="interview_post" class="interview_post" style="display: none;">インタビューを送信しました！</div>
             <img class="sticky" align="right" src="{{ asset('img/mic1.png') }}" width="50px" height="50px"
                  @if(Auth::check())
                     onclick="query_post_modal('{{ $sample_user->logo }}', '{{ $sample_user->name }}', '{{ $sample_user->id }}')"
