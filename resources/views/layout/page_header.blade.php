75533b34fd3c5b6dee1db7b1109ea98dae8ca392
jiuhejong <hokelucpy@hotmail.com>
2019-09-21 07:50:40 +0800

    0920-31

--- resources/views/layout/page_header.blade.php
+++ resources/views/layout/page_header.blade.php
@@ -5,13 +5,21 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        {{--<meta name="twitter:card" content="summary" />--}}
+        {{--<meta name="twitter:site" content="@'@if(Auth::check()) {{ $user->name }} @endif'" />--}}
+        {{--<meta name="twitter:creator" content="@'@if(Auth::check()) {{ $user->name }} @endif'" />--}}
+        {{--<meta property="og:url" content="'@if(Auth::check()) {{ $user->url }} @endif'" />--}}
+        {{--<meta property="og:title" content="Tweet from WHO" />--}}
+        {{--<meta property="og:description" content="プロフ作りました" />--}}
+        {{--<meta property="og:image" content="'@if(Auth::check()) {{ $user->logo }} @endif'" />--}}
+
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:site" content="@'@if(Auth::check()) {{ $user->name }} @endif'" />
-        <meta name="twitter:creator" content="@'@if(Auth::check()) {{ $user->name }} @endif'" />
-        <meta property="og:url" content="'@if(Auth::check()) {{ $user->url }} @endif'" />
+        <meta name="twitter:site" content="@jiuhejong" />
+        <meta name="twitter:creator" content="@jiuhejong" />
+        <meta property="og:url" content="who-japnan.com/query_view/1" />
         <meta property="og:title" content="Tweet from WHO" />
         <meta property="og:description" content="プロフ作りました" />
-        <meta property="og:image" content="'@if(Auth::check()) {{ $user->logo }} @endif'" />
+        <meta property="og:image" content="http://pbs.twimg.com/profile_images/1088202672857964549/IO_0O6ZS.jpg" />
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         {{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>--}}
