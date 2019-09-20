c8721533a985825314b37f4df2ef199381141df8
jiuhejong <hokelucpy@hotmail.com>
2019-09-21 04:08:33 +0800

    0920-28

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -75,6 +75,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         \App\Http\Middleware\Authenticate::class,
+        \App\Http\Middleware\GetHeaderInfo::class,
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
         \Illuminate\Auth\Middleware\Authorize::class,
