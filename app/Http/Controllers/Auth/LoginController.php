82f904426cca298f715a912bae21d2a980ecfbce
jiuhejong <hokelucpy@hotmail.com>
2019-09-21 01:16:32 +0800

    0920-29

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,8 +40,8 @@ class LoginController extends Controller
     {
         return redirect()->route('twittlogin');
     }
-    public function logout()
-    {
-        return redirect()->route('twittlogin');
-    }
+//    public function logout()
+//    {
+//        return redirect()->route('twittlogin');
+//    }
 }
