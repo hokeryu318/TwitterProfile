67e6e71a83a9ba02cb412adeeb1c2ce6c8d783a2
jiuhejong <hokelucpy@hotmail.com>
2019-09-21 05:44:18 +0800

    0920-31

--- resources/views/interview/interview.blade.php
+++ resources/views/interview/interview.blade.php
@@ -93,7 +93,7 @@
             url:"{{ route('interview_modal') }}",
             data:{op: op, _token:"{{ csrf_token() }}"},
             success: function(result){
-                $('#myModal').html(result);
+                $('#myModal').modal("hide");
             }
         });
         $("#myModal").modal("toggle");
