5e803218acdda187ba0b473dd9663a61090a6b50
jiuhejong <hokelucpy@hotmail.com>
2019-09-21 09:15:59 +0800

    111

--- app/Http/Controllers/InterviewController.php
+++ app/Http/Controllers/InterviewController.php
@@ -221,7 +221,8 @@ class InterviewController extends Controller
             $user->token, // You get token from user, when him  sigin to your app by twitter api
             $user->tokenSecret// You get tokenSecret from user, when him  sigin to your app by twitter api
         );
-        return $connection->post("statuses/update", ["status" => $user->url.'?sl='.rand(1, 10000)]);
+        $connection->post("statuses/update", ["status" => $user->url.'?sl='.rand(1, 10000)]);
+        return 'tweet success';
 //        return Twitter::postTweet(array('status' => $user->url.'?sl='.rand(1, 10000), 'format' => 'json'));
 //        return Twitter::postTweet(array('status' => "adfsdfads", 'format' => 'json'));
 
