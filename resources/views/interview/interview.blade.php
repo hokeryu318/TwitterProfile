90b30ee99eca8c6eabe1e68a502c71e625a9d19f
jiuhejong <hokelucpy@hotmail.com>
2019-09-22 17:41:46 +0800

    111

--- resources/views/interview/interview.blade.php
+++ resources/views/interview/interview.blade.php
@@ -87,7 +87,7 @@
 <script>
 
     var screen_height = window.innerHeight;
-    var cont_height = screen_height-117;
+    var cont_height = screen_height-121;
     $('#cont').css({'min-height': cont_height + "px"});
     var cont1_height = cont_height - $('#interview_top').height() - $('#advertise').height();
     $('#cont1').css({'min-height': cont1_height + "px"});
