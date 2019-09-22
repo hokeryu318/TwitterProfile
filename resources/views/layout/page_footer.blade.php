a3837de7a4940fa864e0536216c0c2fe77e9f3a1
jiuhejong <hokelucpy@hotmail.com>
2019-09-22 12:27:40 +0800

    0921

--- resources/views/layout/page_footer.blade.php
+++ resources/views/layout/page_footer.blade.php
@@ -1,18 +1,22 @@
 
-        <div class="container text-center min-hg-70" style="position: relative; bottom: -15px;">
+        <div class="container text-center min-hg-70" style="position: relative; bottom: 0px;">
             <div class="row">
                 <div class="col-sm-4"></div>
                 <div class="col-sm-4 border_top">
                     <div class="bottom_cont">
                         {{--<span onclick="window.location='{{ route("twittlogin") }}'">トップへ戻る</span>--}}
                         <div style="height: 8px;"></div>
-                        <span class="fs-10">よくある質問</span>
-                        <span class="fs-10">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-                        <span class="fs-10">利用規約</span>
-                        <span class="fs-10">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
-                        <span class="fs-10">プライバシーポリシー</span>
+                        <div>
+                            <span class="fs-10">よくある質問</span>
+                            <span class="fs-10">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+                            <span class="fs-10">利用規約</span>
+                            <span class="fs-10">&nbsp;&nbsp;|&nbsp;&nbsp;</span>
+                            <span class="fs-10">プライバシーポリシー</span>
+                        </div>
                         <br>
-                        <span class="fs-10" style="float: left;">&copy 2019 WHO. ALL RIGHTS RESERVED</span>
+                        <div>
+                            <span class="fs-10" style="float: left;">&copy 2019 WHO. ALL RIGHTS RESERVED</span>
+                        </div>
                     </div>
                 </div>
                 <div class="col-sm-4"></div>
