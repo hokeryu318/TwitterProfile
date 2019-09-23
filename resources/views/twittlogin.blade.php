9db53aa446e37c62eff1437f66c5167410ce1749
jiuhejong <hokelucpy@hotmail.com>
2019-09-23 20:47:21 +0800

    222

--- resources/views/twittlogin.blade.php
+++ resources/views/twittlogin.blade.php
@@ -23,7 +23,7 @@
                         <img src="{{ asset('img/man2.png') }}">
                         <img src="{{ asset('img/man1.png') }}">
                     @else
-                        <img src="{{ $user->logo }}" style="width: 70px;border-radius: 50%;" onclick="window.location='{{ url("interview") }}'">
+                        <img src="{{ asset('logo/'.$user->logo) }}" style="width: 70px;border-radius: 50%;" onclick="window.location='{{ url("interview") }}'">
                     @endif
                 </div>
                 <div class="sp-2"></div>
