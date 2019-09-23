8e72b7853a515f64f298e4f503c25bd249d7913a
jiuhejong <hokelucpy@hotmail.com>
2019-09-23 22:15:29 +0800

    123

--- resources/views/layout/page_header.blade.php
+++ resources/views/layout/page_header.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    @if(url()->current().contains == 'query_view')
+    @if(url()->current().containsString('query_view'))
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@jiuhejong" />
         <meta name="twitter:creator" content="@jiuhejong" }} />
